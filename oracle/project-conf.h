3c2e2cdeb333a5586c0eba02170161311b3d29e8
BrukT <bruktekalgne@gmail.com>
2019-11-19 20:48:46 +0100

    added the parameter values we have to adjust with experiment

--- oracle/project-conf.h
+++ oracle/project-conf.h
@@ -7,10 +7,19 @@
 #undef COAP_MAX_OPEN_TRANSACTIONS // Set the max number of concurrent transactions
 #define COAP_MAX_OPEN_TRANSACTIONS 4
 #undef NBR_TABLE_CONF_MAX_NEIGHBORS // Set the max number of entries in neighbors table
-#define NBR_TABLE_CONF_MAX_NEIGHBORS 10
+#define NBR_TABLE_CONF_MAX_NEIGHBORS 15
 #undef UIP_CONF_MAX_ROUTES // Set the max number of routes handled by the node
-#define UIP_CONF_MAX_ROUTES 10
+#define UIP_CONF_MAX_ROUTES 15
 #undef UIP_CONF_BUFFER_SIZE // Set the amount of memory reserved to the uIP packet buffer
 #define UIP_CONF_BUFFER_SIZE 280
+
+#undef RPL_CONF_DIO_REDUNDANCY
+#define RPL_CONF_DIO_REDUNDANCY 15
+
+#undef RPL_CONF_DIO_INTERVAL_MIN
+#define RPL_CONF_DIO_INTERVAL_MIN 10
+
+#undef RPL_CONF_DIO_INTERVAL_DOUBLINGS 
+#define RPL_CONF_DIO_INTERVAL_DOUBLINGS 25
 #endif
 
