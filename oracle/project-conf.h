70fdffda9877fe55855c3b67928430f66e08bf44
giomba <giomba@glgprograms.it>
2019-12-15 11:36:11 +0100

    [src] merge conflict error solved

--- oracle/project-conf.h
+++ oracle/project-conf.h
@@ -4,26 +4,6 @@
 /* Set the max response payload before fragmentation */
 #undef REST_MAX_CHUNK_SIZE
 #define REST_MAX_CHUNK_SIZE 64
-<<<<<<< HEAD
-#undef COAP_MAX_OPEN_TRANSACTIONS // Set the max number of concurrent transactions
-#define COAP_MAX_OPEN_TRANSACTIONS 4
-#undef NBR_TABLE_CONF_MAX_NEIGHBORS // Set the max number of entries in neighbors table
-#define NBR_TABLE_CONF_MAX_NEIGHBORS 15
-#undef UIP_CONF_MAX_ROUTES // Set the max number of routes handled by the node
-#define UIP_CONF_MAX_ROUTES 15
-#undef UIP_CONF_BUFFER_SIZE // Set the amount of memory reserved to the uIP packet buffer
-#define UIP_CONF_BUFFER_SIZE 280
-
-#undef RPL_CONF_DIO_REDUNDANCY
-#define RPL_CONF_DIO_REDUNDANCY 15
-
-#undef RPL_CONF_DIO_INTERVAL_MIN
-#define RPL_CONF_DIO_INTERVAL_MIN 10
-
-#undef RPL_CONF_DIO_INTERVAL_DOUBLINGS 
-#define RPL_CONF_DIO_INTERVAL_DOUBLINGS 25
-=======
-
 /* Set the max number of concurrent transactions */
 #undef COAP_MAX_OPEN_TRANSACTIONS
 #define COAP_MAX_OPEN_TRANSACTIONS 2
@@ -50,6 +30,5 @@
 #undef RPL_CONF_DIO_INTERVAL_DOUBLINGS
 #define RPL_CONF_DIO_INTERVAL_DOUBLINGS 8
 
->>>>>>> rplfix
 #endif
 
